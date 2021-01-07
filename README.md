8ad44d611ce37fcacd6f5e0d8536048c4325c4e9
cdhcrs <42325042+cdhcrs@users.noreply.github.com>
2021-01-07 16:05:31 +0800

    创建

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# demo
+# SpringCloud OAuth2 JWT 多登陆方式
+## 使用方法
+* 启动eureka_server、auth、info、gateway服务
+* 访问http://localhost:8083/auth
  
--- info/src/main/java/com/example/info/config/ResouceServerConfig.java
+++ info/src/main/java/com/example/info/config/ResouceServerConfig.java
@@ -1,6 +1,6 @@
 package com.example.info.config;
 
-import com.example.info.R;
+import com.example.common.utils.R;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
